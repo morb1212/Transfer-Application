d2b32d90d303f37dd36317522674155d4c23c78b
mor <morberger444@gmail.com>
2024-03-04 23:03:33 +0200

    work

--- Sender.py
+++ Sender.py
@@ -1,6 +1,7 @@
 import socket
 import time
 
+
 def client_function():
     # Define host and port
     HOST = '127.0.0.1'
@@ -23,23 +24,13 @@ def client_function():
             FILE_PATH = "received_file.txt"
             client_socket.sendall(FILE_PATH.encode())
 
-            # Open the file and read its contents
-            with open(FILE_PATH, 'rb') as file:
-                file_data = file.read()
-
-            # Send the file data size to the receiver
-            file_data_size = len(file_data)
-            client_socket.sendall(str(file_data_size).encode())
-
-            # Send the file data to the receiver
-            client_socket.sendall(file_data)
-
             print("File transfer completed")
         else:
             print("Unknown input. Please enter 'Y' or 'N'.")
 
     # Close the connection
     client_socket.close()
+
 
 # Run client function directly when this file is executed
 if __name__ == "__main__":
